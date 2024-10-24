812c6646f78b1d328417c19ce889608da9606a35
madina0000 <114565868+madina0000@users.noreply.github.com>
2024-10-24 23:45:20 +0600

    Update index.php

--- Building-Web-Applications-in-PHP/Assignment-08/index.php
+++ Building-Web-Applications-in-PHP/Assignment-08/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Ivan Neradovic - Broken Rock Paper Scissors</title>
+		<title>Madina Yerkebay- Broken Rock Paper Scissors</title>
 		<?php require_once "bootstrap.php"; ?>
 	</head>
 	<body>
